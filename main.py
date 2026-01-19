cf1010ee1c4eaec4af8034ffa8a961d73d1ebf90
SuleymanSade <suleymansade09@gmail.com>
2026-01-19 13:08:28 -0800

    WIP

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import numpy as np
 import cv2
 from pupil_apriltags import Detector
 
+
 class Camera():
     def __init__(self):
-        self.cam = cv2.VideoCapture(0)
+        self.cam = cv2.VideoCapture(1)
         if(not self.cam.isOpened()):
             print("Camera not found or cannot be opened.")
         
         self.detector = Detector(
-            families="tag16h5",
+            families="tag36h11", # default, FRC uses tag36h11
             nthreads=4,
-            quad_decimate=1.0,
-            quad_sigma=0.0,
-            refine_edges=1,
-            decode_sharpening=0.25,
-            debug=0
+            # quad_decimate=1.0,
+            # quad_sigma=0.0,
+            # refine_edges=1,
+            # decode_sharpening=0.25,
+            # debug=0
         )
 
         # Camera parameters (fx, fy, cx, cy)
@@ -24,27 +25,39 @@ class Camera():
         self.tag_size_meters = 0.1651 #in meters
 
     def detect(self):
-        ret, frame = self.cam.read()
-        if not ret:
-            print("Failed to grab frame")
-            return
+        while True:
+            ret, frame = self.cam.read()
+            if not ret:
+                print("Failed to grab frame")
+                return
+            # print(frame)
+            
 
-        # Needs gray for detections
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Needs gray for detections
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            cv2.imshow("Camera Frame", frame)
 
-        # Detect AprilTags
-        detections = self.detector.detect(
-            gray,
-            estimate_tag_pose=True,
-            camera_params=self.cam_params,
-            tag_size=self.tag_size_meters
+            # Detect AprilTags
+            # detections = self.detector.detect(
+            #     gray,
+            #     estimate_tag_pose=True,
+            #     camera_params=self.cam_params,
+            #     tag_size=self.tag_size_meters
 
-        )
+            # )
 
-        for det in detections:
-            print(f"Detected Tag ID: {det.tag_id}")
-            print(f"Pose (X,Y,Z): {det.pose_t.flatten()}")
+            # for det in detections:
+            #     print(f"Detected Tag ID: {det.tag_id}")
+            #     print(f"Pose (X,Y,Z): {det.pose_t.flatten()}")
+        self.cam.release()
+        cv2.destroyAllWindows()
+        
 
 
 cam = Camera()
+# while True:
+    
 cam.detect()
+cam.cam.release()
+cv2.destroyAllWindows()
