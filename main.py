56aeff79428afd51fb4229b649d80eeae4fb0448
SuleymanSade <suleymansade09@gmail.com>
2026-01-19 10:53:00 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import cv2
+from pupil_apriltags import Detector
+
+class Camera():
+    def __init__(self):
+        self.cam = cv2.VideoCapture(0)
+        if(not self.cam.isOpened()):
+            print("Camera not found or cannot be opened.")
+        
+        self.detector = Detector(
+            families="tag16h5",
+            nthreads=4,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0
+        )
+
+        # Camera parameters (fx, fy, cx, cy)
+        self.cam_params = (600, 600, 320, 240)
+        self.tag_size_meters = 0.1651 #in meters
+
+    def detect(self):
+        ret, frame = self.cam.read()
+        if not ret:
+            print("Failed to grab frame")
+            return
+
+        # Needs gray for detections
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect AprilTags
+        detections = self.detector.detect(
+            gray,
+            estimate_tag_pose=True,
+            camera_params=self.cam_params,
+            tag_size=self.tag_size_meters
+
+        )
+
+        for det in detections:
+            print(f"Detected Tag ID: {det.tag_id}")
+            print(f"Pose (X,Y,Z): {det.pose_t.flatten()}")
+
+
+cam = Camera()
+cam.detect()
